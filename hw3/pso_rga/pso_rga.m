e1cbf8ca74a96ba19d376855908d3d2cb5393273
batteryouo <oyinggaio@gmail.com>
2025-09-30 04:46:26 +0800

    feat: complete pso rga

---
+++ hw3/pso_rga/pso_rga.m
@@ -0,0 +1,29 @@
+clear;clc;
+
+global Kp Ki Kd t y;
+
+fitFunction=@psoRGAFitFunction;
+generation = 30;
+particleSize = 30;
+particleInfoSize = [2, 3];
+mutation_rate = 0.05;
+
+initialParticle = 2 * rand( particleSize, particleInfoSize(1), particleInfoSize(2) );
+initialParticle(:, 2, :) = 1 * rand(particleSize, 1, 3);
+
+bestParticle = pso_rga_pid(initialParticle, generation, mutation_rate, fitFunction, true);
+
+figure()
+sim('pso_rga_PID');
+plot(t,y)
+title('Step Response')
+xlabel('time')
+ylabel('y')
+hold on
+
+figure()
+plot(t,u)
+title('Control Energy')
+xlabel('time')
+ylabel('u')
+hold on
--- hw3/pso_rga/pso_rga_pid.m
+++ hw3/pso_rga/pso_rga_pid.m
@@ -1,4 +1,4 @@
-function [bestParticle] = pso_ep_pid(initialParticle, generation, mutation_rate, fitFunction, show_fitness)
+function [bestParticle] = pso_rga_pid(initialParticle, generation, mutation_rate, fitFunction, show_fitness)
     
     particleSize = size(initialParticle, 1);
     particle = initialParticle(:, :, :);
@@ -36,13 +36,33 @@ function [bestParticle] = pso_ep_pid(initialParticle, generation, mutation_rate,
         
         particle(:, 2, :) = w*particle(:, 2, :) + c1*rand()*( pbestParticle - particle(:, 1, :) ) + c2*rand()*( gbestParticle(1, :, :) - particle(:, 1, :) );
         particle(:, 1, :) = particle(:, 1, :) + particle(:, 2, :);
-        %EP
-        mutation_prob = rand(size(particle));
-        mutation_index = mutation_prob < mutation_rate;
-        new_particle = particle;
-        new_particle(mutation_index) = rand([sum(mutation_index, "all"), 1]);
-        newfitness = fitFunction(new_particle(:, 1, :));
-        particle(newfitness > fitness) = new_particle(newfitness > fitness);
+        %RGA
+        fitness_prob_cum = cumsum(fitness/sum(fitness));
+        parent_index = arrayfun(@(x) find(fitness_prob_cum > x, 1, 'first'), rand(particleSize, 1));
+        new_particle = particle; 
+
+        %crossover
+        for j = 1:2:particleSize-1
+            p1 = parent_index(j);
+            p2 = parent_index(j+1);
+            crossover_prob = rand(1, 3);
+            parent1 = reshape(particle(p1, 1, :), [1, 3]);
+            parent2 = reshape(particle(p2, 1, :), [1, 3]);
+            child1 = crossover_prob .* parent1 + (1-crossover_prob) .* parent2;
+            child2 = (1-crossover_prob) .* parent1 + crossover_prob .* parent2;
+            new_particle(p1, 1, :) = child1;
+            new_particle(p2, 1, :) = child2;
+        end
+        %mutation
+        mutation_mask = rand(particleSize, 3) < mutation_rate;
+        mutation_values = rand(sum(mutation_mask, "all"), 1);
+        tmp_pos = new_particle(:,1,:);
+        tmp_pos(mutation_mask) = mutation_values;
+        new_particle(:,1,:) = tmp_pos;
+        %selection
+        new_fitness = fitFunction(new_particle(:, 1, :));
+        replace_mask = new_fitness > fitness;
+        particle(replace_mask, 1, :) = new_particle(replace_mask, 1, :);    
 
     end
 
